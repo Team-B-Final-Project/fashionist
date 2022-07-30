87d5e217b25663204c50c9f39829658fedabfe91
aris <ariskusnul@gmail.com>
2022-07-30 19:15:13 +0700

    first commit

---
+++ src/main/java/com/anbit/fashionist/FashionistApplication.java
@@ -0,0 +1,13 @@
+package com.anbit.fashionist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FashionistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FashionistApplication.class, args);
+	}
+
+}
