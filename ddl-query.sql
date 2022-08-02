3b6d8b4e2ce314ed6607436ad300451e89e671e3
najibdj27 <najib.djulfikar@gmail.com>
2022-08-02 10:51:52 +0700

    create ddl for address and user_address

--- ddl-query.sql
+++ ddl-query.sql
@@ -26,3 +26,26 @@ CREATE TABLE public.user (
     CONSTRAINT "phone_number_ukey" UNIQUE ("phone_number"),
     CONSTRAINT "profile_picture_fkey" FOREIGN KEY ("profile_picture_id") REFERENCES public.profile_picture("id")
 );
+
+CREATE TABLE public.address (
+    "id" bigserial,
+    "name" varchar(50) NOT NULL,
+    "phone_number" char(12) NOT NULL,
+    "province" varchar(50) NOT NULL,
+    "city" varchar(50) NOT NULL,
+    "district" varchar(50) NOT NULL,
+    "full_address" varchar NOT NULL,
+    "created_at" timestamp NOT NULL DEFAULT NOW(),
+    "updated_at" timestamp NOT NULL DEFAULT NOW(),
+    "creatd_by" varchar NOT NULL,
+    "updated_by" varchar NOT NULL,
+    CONSTRAINT "address_pkey" PRIMARY KEY ("id")
+);
+
+CREATE TABLE public.user_address (
+    "user_id" bigint NOT NULL,
+    "address_id" bigint NOT NULL,
+    CONSTRAINT "user_address_pkey" PRIMARY KEY ("user_id", "address_id"),
+    CONSTRAINT "user_fkey" FOREIGN KEY ("user_id") REFERENCES public.user("id"),
+    CONSTRAINT "address_fkey" FOREIGN KEY ("address_id") REFERENCES public.address("id")
+);
