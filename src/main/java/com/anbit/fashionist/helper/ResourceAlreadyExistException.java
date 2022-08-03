20dbdbec71f2050afb7601cd5ff26498f4141715
najibdj27 <najib.djulfikar@gmail.com>
2022-08-03 10:56:59 +0700

    Add helper

---
+++ src/main/java/com/anbit/fashionist/helper/ResourceAlreadyExistException.java
@@ -0,0 +1,9 @@
+package com.anbit.fashionist.helper;
+
+public class ResourceAlreadyExistException extends Throwable {
+
+    public ResourceAlreadyExistException(String message) {
+        super(message);
+    }
+    
+}
