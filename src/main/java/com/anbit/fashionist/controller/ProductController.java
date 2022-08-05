ea5d2bd095d3e81f2141f89b6b39e59a86895eb1
najibdj27 <najib.djulfikar@gmail.com>
2022-08-05 22:04:38 +0700

    Fix product controller

--- src/main/java/com/anbit/fashionist/controller/ProductController.java
+++ src/main/java/com/anbit/fashionist/controller/ProductController.java
@@ -1,13 +1,11 @@
 package com.anbit.fashionist.controller;
 
 
-import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@AllArgsConstructor
-@RequestMapping("/api")
+@RequestMapping("/api/v1")
 public class ProductController {
 
 }
