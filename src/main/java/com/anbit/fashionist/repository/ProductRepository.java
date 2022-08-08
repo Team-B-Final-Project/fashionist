7fe5e79da0d8e912ba978814dea4e0521e4c91d4
najibdj27 <najib.djulfikar@gmail.com>
2022-08-08 10:57:35 +0700

    Remove unused import

--- src/main/java/com/anbit/fashionist/repository/ProductRepository.java
+++ src/main/java/com/anbit/fashionist/repository/ProductRepository.java
@@ -6,8 +6,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Integer> {
     Page<Product> findByName(String keyword, Pageable pageable);
