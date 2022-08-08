762e7fa60c52b97e98d984f5a07b7df276257489
najibdj27 <najib.djulfikar@gmail.com>
2022-08-09 06:39:12 +0700

    Add missing required annotation

--- src/main/java/com/anbit/fashionist/repository/ProductTransactionRepository.java
+++ src/main/java/com/anbit/fashionist/repository/ProductTransactionRepository.java
@@ -3,10 +3,12 @@ package com.anbit.fashionist.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.anbit.fashionist.domain.dao.ProductTransaction;
 import com.anbit.fashionist.domain.dao.Transaction;
 
+@Repository
 public interface ProductTransactionRepository extends JpaRepository<ProductTransaction, Long> {
     List<ProductTransaction> findByTransaction(Transaction transaction);
 }
--- src/main/java/com/anbit/fashionist/repository/TransactionStatusRepository.java
+++ src/main/java/com/anbit/fashionist/repository/TransactionStatusRepository.java
@@ -3,10 +3,12 @@ package com.anbit.fashionist.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.anbit.fashionist.constant.ETransactionStatus;
 import com.anbit.fashionist.domain.dao.TransactionStatus;
 
+@Repository
 public interface TransactionStatusRepository extends JpaRepository<TransactionStatus, Integer> {
     Optional<TransactionStatus> findByName(ETransactionStatus name);
 }
