5dcb340af92683628fa1241b2f3dc944a9ee986f
najibdj27 <najib.djulfikar@gmail.com>
2022-08-09 19:35:52 +0700

    fix cart bugs

---
+++ src/main/java/com/anbit/fashionist/domain/dto/AddCartRequestDTO.java
@@ -0,0 +1,17 @@
+package com.anbit.fashionist.domain.dto;
+
+
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+public class AddCartRequestDTO {
+    private Long productId;
+
+    private Integer itemUnit;
+
+    private Float totalPrice;
+}
