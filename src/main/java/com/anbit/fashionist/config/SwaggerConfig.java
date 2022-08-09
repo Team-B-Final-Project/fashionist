751f4dc6a15e9124dc627e5c4b058e7a04c4ac73
najibdj27 <najib.djulfikar@gmail.com>
2022-08-09 09:29:20 +0700

    create swagger config

--- src/main/java/com/anbit/fashionist/config/SwaggerConfig.java
+++ src/main/java/com/anbit/fashionist/config/SwaggerConfig.java
@@ -3,11 +3,14 @@ package com.anbit.fashionist.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 
 @Configuration
+@OpenAPIDefinition(info=@Info(title="Fashionist OpenAPI", version = "${com.anbit.fashionist.project-version}", description = "E-commerce platform that is selling products of fashion in Indonesia."))
 public class SwaggerConfig {
     @Bean
     public OpenAPI customOpenAPI() {
