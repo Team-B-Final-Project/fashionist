03d88854624ab04e8d587b6b83636484533b4f7a
najibdj27 <najib.djulfikar@gmail.com>
2022-08-09 08:29:54 +0700

    Add summary for every endpoint and add desciption for controller

--- src/main/java/com/anbit/fashionist/controller/AuthController.java
+++ src/main/java/com/anbit/fashionist/controller/AuthController.java
@@ -5,8 +5,13 @@ import com.anbit.fashionist.domain.dto.SignUpRequestDTO;
 import com.anbit.fashionist.helper.ResourceAlreadyExistException;
 import com.anbit.fashionist.helper.ResourceNotFoundException;
 import com.anbit.fashionist.service.AuthServiceImpl;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +19,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 
+@Tag(name = "1. Auth Controller")
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/api/v1/auth")
 public class AuthController {
@@ -21,10 +28,11 @@ public class AuthController {
     AuthServiceImpl authServiceImpl;
 
     /***
-     * Endpoint to sign in and get the token for accessing the CRUD system
+     * Sign in and get the token for access
      * @param loginRequest
      * @return
      */
+    @Operation(summary = "Sign in and get the token for access")
     @PostMapping("/signin")
     public ResponseEntity<?> authenticate(@Valid @RequestBody SignInRequestDTO signInRequest) throws ResourceNotFoundException
     {
@@ -32,11 +40,12 @@ public class AuthController {
     }
 
     /***
-     * Endpoint for register user 
+     * Register for user
      * @param signUpRequestDTO
      * @return
      * @throws ResourceAlreadyExistException
      */
+    @Operation(summary = "Register for user")
     @PostMapping("/signup")
     public ResponseEntity<?> register(@Valid @RequestBody SignUpRequestDTO signUpRequestDTO) throws ResourceAlreadyExistException {
         return authServiceImpl.registerUser(signUpRequestDTO);
--- src/main/java/com/anbit/fashionist/controller/StoreController.java
+++ src/main/java/com/anbit/fashionist/controller/StoreController.java
@@ -12,12 +12,24 @@ import com.anbit.fashionist.helper.ResourceAlreadyExistException;
 import com.anbit.fashionist.helper.ResourceNotFoundException;
 import com.anbit.fashionist.service.StoreServiceImpl;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Tag(name = "3. Store Controller")
 @RestController
 @RequestMapping("/api/v1")
 public class StoreController {
     @Autowired
     StoreServiceImpl storeService;
 
+    /***
+     * Open store for user who doesn't have any store yet
+     * @param requestDTO
+     * @return
+     * @throws ResourceNotFoundException
+     * @throws ResourceAlreadyExistException
+     */
+    @Operation(summary = "Open store for user who doesn't have any store yet")
     @PostMapping("/store/create")
     @PreAuthorize("hasAuthority('ROLE_CUSTOMER')")
     public ResponseEntity<?> openStore(CreateStoreRequestDTO requestDTO) throws ResourceNotFoundException, ResourceAlreadyExistException {
