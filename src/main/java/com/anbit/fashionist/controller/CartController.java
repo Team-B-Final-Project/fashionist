4b82ade7d00efdd608f86759dc9e909a0c0f28a0
najibdj27 <najib.djulfikar@gmail.com>
2022-08-10 12:54:36 +0700

    add authorization to cart controller

--- src/main/java/com/anbit/fashionist/controller/CartController.java
+++ src/main/java/com/anbit/fashionist/controller/CartController.java
@@ -6,15 +6,21 @@ import com.anbit.fashionist.domain.dto.EditCartTotalItemRequestDTO;
 import com.anbit.fashionist.helper.ResourceNotFoundException;
 import com.anbit.fashionist.service.CartServiceImpl;
 
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 
+@Tag(name = "5. Cart Controller")
 @RestController
 @RequestMapping("/api/v1")
+@SecurityRequirement(name = "bearer-key")
 public class CartController {
     @Autowired
     CartServiceImpl cartService;
@@ -26,10 +32,11 @@ public class CartController {
      * @throws ResourceNotFoundException
      */
     @PostMapping("/add/cart")
+    @PreAuthorize("hasAuthority('ROLE_CUSTOMER')")
     public ResponseEntity<?> add(@Valid AddCartRequestDTO requestDTO) throws ResourceNotFoundException {
         return cartService.addCart(requestDTO);
     }
-
+    
     /***
      * Edit product in cart
      * @param cartRequestDTO
@@ -37,10 +44,11 @@ public class CartController {
      * @throws ResourceNotFoundException
      */
     @PutMapping("/edit/cart")
+    @PreAuthorize("hasAuthority('ROLE_CUSTOMER')")
     public ResponseEntity<?> edit(EditCartTotalItemRequestDTO requestDTO) throws ResourceNotFoundException {
         return cartService.editCartTotalItem(requestDTO);
     }
-
+    
     /***
      * Delete product from cart
      * @param id
@@ -48,6 +56,7 @@ public class CartController {
      * @throws ResourceNotFoundException
      */
     @DeleteMapping("/delete/cart/{id}")
+    @PreAuthorize("hasAuthority('ROLE_CUSTOMER')")
     public ResponseEntity<?> delete(@PathVariable Long id) throws ResourceNotFoundException {
         return cartService.deleteCart(id);
     }
