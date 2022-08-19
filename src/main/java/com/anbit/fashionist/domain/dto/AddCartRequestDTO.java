dca81df6ab640489c40a3eea68396a1bb9ccd8e8
najibdj27 <najib.djulfikar@gmail.com>
2022-08-19 16:08:30 +0700

    validation addcart done

--- src/main/java/com/anbit/fashionist/domain/dto/AddCartRequestDTO.java
+++ src/main/java/com/anbit/fashionist/domain/dto/AddCartRequestDTO.java
@@ -1,7 +1,7 @@
 package com.anbit.fashionist.domain.dto;
 
 
-import javax.validation.constraints.Size;
+import javax.validation.constraints.*;
 
 import lombok.*;
 
@@ -11,8 +11,14 @@ import lombok.*;
 @Setter
 @Builder
 public class AddCartRequestDTO {
+    @NotNull
+    @NotBlank
+    @Pattern(regexp = "[1-9]")
     private Long productId;
 
-    @Size(min = 1)
+    @Positive
+    @Pattern(regexp = "[1-9]")
+    @Max(1000)
+    @NotNull
     private Integer itemUnit;
 }
