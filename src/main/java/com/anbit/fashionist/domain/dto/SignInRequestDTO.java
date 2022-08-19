d0a122b0879388af37f65788d4463c26ac91a3c4
najibdj27 <najib.djulfikar@gmail.com>
2022-08-19 10:33:01 +0700

    SignIn validation done

--- src/main/java/com/anbit/fashionist/domain/dto/SignInRequestDTO.java
+++ src/main/java/com/anbit/fashionist/domain/dto/SignInRequestDTO.java
@@ -1,8 +1,10 @@
 package com.anbit.fashionist.domain.dto;
 
-import lombok.*;
-
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+import lombok.*;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -12,7 +14,12 @@ import javax.validation.constraints.NotBlank;
 
 public class SignInRequestDTO {
     @NotBlank
+    @Size(min = 3, max = 20)
+    @Pattern(regexp = "(?![_.]).+", message = "can't contain '_' at the beginning")
+    @Pattern(regexp = "(?!.*[_.]{2}).+", message = "can't contain double '_'")
+    @Pattern(regexp = "[a-zA-Z0-9._]+(?<![.])", message = "can only contain letters, numbers and underscore")
     private String username;
+    
     @NotBlank
     private String password;
 }
