0b8839bc155a4c68d95b2d6ce0f99483f1a31178
billymarcelino058 <billymarcelino058@gmail.com>
2022-08-23 10:00:30 +0700

    category leveling

--- src/main/java/com/anbit/fashionist/domain/dao/Product.java
+++ src/main/java/com/anbit/fashionist/domain/dao/Product.java
@@ -3,7 +3,9 @@ package com.anbit.fashionist.domain.dao;
 import com.anbit.fashionist.domain.common.Audit;
 import lombok.*;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import javax.persistence.*;
 
@@ -39,12 +41,17 @@ public class Product extends Audit {
     @Column(name = "stock", nullable = false)
     private Integer stock;
 
-    @ManyToOne(fetch = FetchType.LAZY, targetEntity = Category.class)
-    @JoinColumn(name = "category_id")
-    private Category category;
+//    @ManyToOne(fetch = FetchType.LAZY, targetEntity = Category.class)
+//    @JoinColumn(name = "category_id")
+//    private Category category;
 
     @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "product")
     private List<ProductTransaction> productTransactions;
+
+    @Builder.Default
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "product_category", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
+    private Set<Category> roles = new HashSet<>();
 
     @Override
     public String toString() {
@@ -56,7 +63,7 @@ public class Product extends Audit {
                 ", description='" + description + '\'' +
                 ", price=" + price +
                 ", stock=" + stock +
-                ", categoryId=" + category +
+//                ", categoryId=" + category +
                 '}';
     }
 
