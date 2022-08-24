0f18949563e21f8c1b27879902512f61b4acc6df
ariskusnul widitama <ariskusnul@gmail.com>
2022-08-24 20:31:48 +0700

    fix response otp

---
+++ src/main/java/com/anbit/fashionist/domain/dto/ConfirmOTPResponseDTO.java
@@ -0,0 +1,15 @@
+package com.anbit.fashionist.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class ConfirmOTPResponseDTO {
+
+    private String resetPasswordToken;
+}
